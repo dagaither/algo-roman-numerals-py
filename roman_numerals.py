bb6e93f8608dad6f6a62baffbb9ffe9ffd8b352d
David Gaither <gaither@protonmail.com>
2024-02-01 20:47:14 -0500

    Initial commit

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,16 @@
 def to_roman(num):
-    # write your code here!
-    pass
+
+    rom_nums = {
+        "M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90,
+        "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1,
+    }
+
+    result = ""
+    
+    for k, v in rom_nums.items():
+        if num >= v:
+            result = result + k
+            num -= v
+            print(num)
+
+    return result
